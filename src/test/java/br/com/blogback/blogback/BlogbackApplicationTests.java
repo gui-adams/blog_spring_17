61539e510be0d641dcc6ea296d1e8c33c5759f91
gui-adams <guilherme.adams@live.com>
2024-11-02 22:37:00 -0300

    init

---
+++ src/test/java/br/com/blogback/blogback/BlogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blogback.blogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
