da10fb2124ca15d2a6ba3cfa531dffd0002ca28b
gui-adams <guilherme.adams@live.com>
2024-11-22 21:31:51 -0300

    v3.2

--- src/main/java/br/com/blogback/blogback/service/CloudinaryService.java
+++ src/main/java/br/com/blogback/blogback/service/CloudinaryService.java
@@ -15,19 +15,26 @@ public class CloudinaryService {
     private final Cloudinary cloudinary;
 
     public CloudinaryService(
-            @Value("${cloudinary.cloud_name}") String cloudName,
-            @Value("${cloudinary.api_key}") String apiKey,
-            @Value("${cloudinary.api_secret}") String apiSecret) {
+            @Value("${backblog.cloudinary.cloud_name}") String cloudName,
+            @Value("${backblog.cloudinary.api_key}") String apiKey,
+            @Value("${backblog.cloudinary.api_secret}") String apiSecret) {
 
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
-                "api_secret", apiSecret));
+                "api_secret", apiSecret
+        ));
     }
 
     public String uploadImage(MultipartFile file) throws IOException {
-        Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(),
-                ObjectUtils.asMap("resource_type", "image"));
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("O arquivo não pode estar vazio!");
+        }
+
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap("resource_type", "image")
+        );
         return uploadResult.get("secure_url").toString(); // Retorna a URL pública da imagem
     }
 }
